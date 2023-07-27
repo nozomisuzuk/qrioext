92eb01f9f88a4d1e45e88557798c806439399e3e
morita761 <112071745+morita761@users.noreply.github.com>
2023-07-27 15:16:23 +0900

    ws url修正

--- debug/normal_debug.sh
+++ debug/normal_debug.sh
@@ -2,7 +2,7 @@
 echo 'Debug start!'
 # NORMAL
 curl http://192.168.2.98:3000/key_server -v -o /dev/null -H 'Cookie: User=qrio_debug; Token=6bb94-eab899e-463e-930789-3b48c13620;' 
-wscat -c ws://192.168.2.98:3000 -H 'Cookie: User=qrio_debug; Token=6bb94-eab899e-463e-930789-3b48c13620;' -w 2 -x "state-check"
+wscat -c ws://192.168.2.98:3030 -H 'Cookie: User=qrio_debug; Token=6bb94-eab899e-463e-930789-3b48c13620;' -w 2 -x "state-check"
 
 # ERROR
-# wscat -c ws://192.168.2.98:3000 -H 'Cookie: User=null; Token=null;' -w 2 -x "state-check"
+# wscat -c ws://192.168.2.98:3030 -H 'Cookie: User=null; Token=null;' -w 2 -x "state-check"
